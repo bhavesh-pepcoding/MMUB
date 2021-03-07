22dfef1addd74091727054cde8a3af906bce37ff
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-03-07 11:50:58 +0530

    ADD v2

--- wcat/script.js
+++ wcat/script.js
@@ -28,6 +28,29 @@ function wcat(cmds) {
         }
     }
 
+    // writing commands
+    if(options.includes("-w")) {
+        if(options.length != 1 || files.length != 2 || cmds.indexOf("-w") != 1) {
+            console.log("Command not found");
+            return;
+        }
+        let data = fs.readFileSync(files[0], "utf-8");
+        fs.writeFileSync(files[1], data);
+        return;
+    }else if(options.includes("-a")) {
+        if(options.length != 1 || files.length != 2 || cmds.indexOf("-a") != 1) {
+            console.log("Command not found");
+            return;
+        }
+        let file1 = fs.readFileSync(files[0], "utf-8");
+        let file2 = fs.readFileSync(files[1], "utf-8");
+        fs.writeFileSync(files[1], file2 + "\r\n" + file1);
+        return;
+    }
+
+    // Reading commands
+    let numbering = 1;
+
     for(i in files) {
         let data = fs.readFileSync(files[i],"utf-8");
         if(options.includes("-s")) {
@@ -35,13 +58,35 @@ function wcat(cmds) {
             // let allText = "";
             for(j in lines) {
                 if(lines[j] != "") {
-                    console.log(lines[j]);
+                    if(options.includes("-n") || options.includes("-b")) {
+                        console.log(numbering + ". " + lines[j]);
+                        numbering += 1;
+                    } else {
+                        console.log(lines[j]);
+                    }
                     // allText += lines[j] + "\n";
                 }
             }
             // console.log(allText);
             // console.log(lines);
-        } else {
+        } else if((options.includes("-n") && !options.includes("-b")) || (options.includes("-n") && options.includes("-b") && (options.indexOf("-n") < options.indexOf("-b")))) {
+            let lines = data.split("\r\n");
+            for(j in lines) {
+               console.log(numbering + ". " + lines[j]);
+               numbering++; 
+            }
+        } else if(options.includes("-b")) {
+            let lines = data.split("\r\n");
+            for(j in lines) {
+                if(lines[j] == "") {
+                    console.log(lines[j]);
+                } else {
+                    console.log(numbering + ". " + lines[j]);
+                    numbering += 1;
+                }
+            }
+        }
+        else {
             console.log(data);
         }
     }
