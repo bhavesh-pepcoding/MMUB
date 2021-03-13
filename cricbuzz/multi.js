ba7b8ccbb681bf23e28e5794424ba7346556d168
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-03-13 14:44:13 +0530

    ADD async multipage

--- cricbuzz/multi.js
+++ cricbuzz/multi.js
@@ -11,6 +11,44 @@ let inningsBatsmen = [];
 let inningsBowler = [];
 let carrerData = [];
 let fs = require('fs');
+let playersAdded = 0;
+async function getCareerData(url, i, totalPlayers) {
+    let browser = new wd.Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build();
+        await browser.get(url);
+        await browser.wait(wd.until.elementLocated(wd.By.css("table")));
+        let tables = await browser.findElements(wd.By.css("table"));
+        for(j in tables) {
+            let data = {};
+            let rows = await tables[j].findElements(wd.By.css("tbody tr"));
+            for(row of rows) {
+                let tempData = {};
+                let columns = await row.findElements(wd.By.css("td"));
+                let matchType = await columns[0].getAttribute("innerText");
+                let keyArr;
+                if(j == 0) {
+                    keyArr = batsmenColumns;
+                }
+                if(j == 1) {
+                    keyArr = bowlerColumns;
+                }
+                for(let k = 1; k < columns.length; k++) {
+                    tempData[keyArr[k-1]] = await columns[k].getAttribute("innerText");
+                }
+                data[matchType] = tempData;
+            }
+            if(j == 0) {
+                carrerData[i]["battingCarrer"] = data;
+            } else {
+                carrerData[i]["bowlingCarrer"] = data;
+            }
+
+        }
+        browser.close();
+        playersAdded += 1;
+        if(playersAdded == totalPlayers) {
+            fs.writeFileSync("career.json", JSON.stringify(carrerData));
+        }
+} 
  
 async function main() {
     await browser.get(`https://www.cricbuzz.com/live-cricket-scores/${matchId}`);
@@ -43,34 +81,9 @@ async function main() {
 
     let finalUrls = inningsBatsmen.concat(inningsBowler);
     for(i in finalUrls) {
-        await browser.get(finalUrls[i]);
-        await browser.wait(wd.until.elementLocated(wd.By.css(".table.cb-col-100.cb-plyr-thead")))
-        let tables = await browser.findElements(wd.By.css(".table.cb-col-100.cb-plyr-thead"));
-        for(j in tables) {
-            let data = {};
-            let rows = await tables[j].findElements(wd.By.css("tbody tr"));
-            for(row of rows) {
-                let tempData = {};
-                let columns = await row.findElements(wd.By.css("td"));
-                let matchType = await columns[0].getAttribute("innerText");
-                let keyArr = batsmenColumns;
-                if(j == 1) {
-                    keyArr = bowlerColumns;
-                }
-                for(let k = 1; k < columns.length; k++) {
-                    tempData[keyArr[k-1]] = await columns[k].getAttribute("innerText");
-                }
-                data[matchType] = tempData;
-            }
-            if(j == 0) {
-                carrerData[i]["battingCarrer"] = data;
-            } else {
-                carrerData[i]["bowlingCarrer"] = data;
-            }
-
-        }
+        getCareerData(finalUrls[i], i, finalUrls.length);
     }
-    fs.writeFileSync("career.json", JSON.stringify(carrerData));
+    browser.close();
 }
 
 main();
