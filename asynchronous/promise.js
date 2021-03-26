c8461e4fed1b4064055051c762731ca522f2ea88
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-03-26 21:05:26 +0530

    promise practice

--- asynchronous/promise.js
+++ asynchronous/promise.js
@@ -104,11 +104,76 @@
 // console.log("hello");
 // console.log("hello2");
 
-function wait(ms) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(ms)
-      }, ms )
-    })
-  }  
+// function wait(ms) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(ms)
+//       }, ms )
+//     })
+//   }  
   
+
+
+// setTimeout(function() {
+//   console.log("timeout")
+// },0);
+// new Promise(function(resolve){
+//   resolve()
+// }).then(function(){
+//   console.log("promise")
+// });
+
+// console.log("hello");
+
+
+// final answer of last promise
+
+let central = require('./central'),
+    db1 = require('./db1'),
+    db2 = require('./db2'),
+    db3 = require('./db3'),
+    vault = require('./vault'),
+    mark = require('./mark');
+
+module.exports = function (id) {
+    // TODO
+    // Reminder: The deadline is tomorrow !
+    return new Promise(function (resolve, reject) {
+        let obj = {};
+        obj["id"] = id;
+        Promise.all([central(id).then(function (data) {
+            if (data == 'db1') {
+                return [db1(id),"db1"];
+            } else if (data == 'db2') {
+                return [db2(id),"db2"];
+            } else if (data == 'db3') {
+                return [db3(id),"db3"]
+            }
+        }).catch(function (err) {
+            reject("Error central")
+        }),
+        vault(id).then(function(data){
+            return data;
+        }).catch(function (data) {
+            reject("Error vault")
+        })]).then(function (arr) {
+            arr[0][0].then(function (data) {
+                    obj['username'] = data.username;
+                    obj['country'] = data.country;
+                     mark(id).then(function () { }).catch(function (err) {
+
+                    });
+                    resolve(obj);
+
+                }).catch(function (err) {
+                    reject("Error " + arr[0][1]);
+                })
+            obj["firstname"] = arr[1].firstname;
+            obj["lastname"] = arr[1].lastname;
+            obj["email"] = arr[1].email;
+        }).catch(function (error) {
+            reject(error)
+
+        })
+    })
+};
