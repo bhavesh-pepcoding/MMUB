a3a3b9b2f2fd4868f1cef81e453ebcc4bb7bf177
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-04-25 19:52:04 +0530

    ADD zira tickets

--- DOM/Zira/script.js
+++ DOM/Zira/script.js
@@ -1,5 +1,6 @@
 let TC = document.querySelector(".ticket-container");
 let allFilters = document.querySelectorAll(".filter");
+let deleteButton = document.querySelector(".delete");
 let modalVisible = false;
 let selectedPriority = "pink";
 
@@ -17,8 +18,9 @@ addButton.addEventListener("click", showModal);
 
 function showModal(e) {
     if(!modalVisible) {
-        let modal = `<div class="modal">
-                    <div class="task-to-be-added" spellcheck="false" data-type="false" contenteditable="true">
+        let modal  = document.createElement("div");
+        modal.classList.add("modal");
+        modal.innerHTML = `<div class="task-to-be-added" spellcheck="false" data-type="false" contenteditable="true">
                         <span class="placeholder">Enter your text here</span>
                     </div>
                     <div class="priority-list">
@@ -26,9 +28,8 @@ function showModal(e) {
                         <div class="blue-modal-filter modal-filter"></div>
                         <div class="green-modal-filter modal-filter"></div>
                         <div class="yellow-modal-filter modal-filter"></div>
-                    </div>
-                </div>`;
-    TC.innerHTML = TC.innerHTML + modal;
+                    </div>`;
+    TC.appendChild(modal);
     selectedPriority = "pink";
     let taskTyper = document.querySelector(".task-to-be-added");
     taskTyper.addEventListener("click", function(e) {
@@ -56,18 +57,32 @@ function selectPriority(e) {
 
 function addTicket(taskTyper,e) {
     if(e.key == "Enter" && e.shiftKey == false && taskTyper.innerText.trim() != "") {
-        let ticket = `<div class="ticket">
-                        <div class="ticket-color ticket-color-${selectedPriority}"></div>
-                        <div class="ticket-id">#abdhjf</div>
-                        <div class="task">
-                            ${taskTyper.innerText}
-                        </div>
+        let ticket = document.createElement("div");
+        ticket.classList.add("ticket");
+        ticket.innerHTML =  `<div class="ticket-color ticket-color-${selectedPriority}"></div>
+                    <div class="ticket-id">#abdhjf</div>
+                    <div class="task">
+                        ${taskTyper.innerText}
                     </div>`;
         document.querySelector(".modal").remove();
         modalVisible = false;
-        TC.innerHTML = TC.innerHTML + ticket;
+        ticket.addEventListener("click", function(e) {
+            if(e.currentTarget.classList.contains("active")) {
+                e.currentTarget.classList.remove("active");
+            } else {
+                e.currentTarget.classList.add("active");
+            }
+        });
+        TC.appendChild(ticket);
     } else if(e.key == "Enter" && e.shiftKey == false) {
         e.preventDefault();
         alert("you have not typed anything");
     }
 }
+
+deleteButton.addEventListener("click", function(e) {
+    let selectedTickets = document.querySelectorAll(".ticket.active").remove();
+    for(let i = 0; i < selectedTickets.length; i++) {
+        selectedTickets[i].remove();
+    } 
+});
