6c46bb9e1d9dfdca577e8b3f3c1a5f4e83cf9b6b
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-05-07 21:37:27 +0530

    ADD fix

--- DOM/excel-clone/script.js
+++ DOM/excel-clone/script.js
@@ -34,6 +34,7 @@ $("#cells").scroll(function(e) {
 });
 
 $(".input-cell").dblclick(function(e) {
+    $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
     $(this).attr("contenteditable", "true");
     $(this).focus();
 });
@@ -68,23 +69,25 @@ $(".input-cell").click(function(e) {
 });
 
 function unselectCell(ele,e,topCell,bottomCell,leftCell,rightCell) {
-    if($(ele).hasClass("top-selected")) {
-        topCell.removeClass("bottom-selected");
+    if($(ele).attr("contenteditable") == "false") {
+        if($(ele).hasClass("top-selected")) {
+            topCell.removeClass("bottom-selected");
+        }
+    
+        if($(ele).hasClass("bottom-selected")) {
+            bottomCell.removeClass("top-selected");
+        }
+    
+        if($(ele).hasClass("left-selected")) {
+            leftCell.removeClass("right-selected");
+        }
+    
+        if($(ele).hasClass("right-selected")) {
+            rightCell.removeClass("left-selected");
+        }
+    
+        $(ele).removeClass("selected top-selected bottom-selected left-selected right-selected");
     }
-
-    if($(ele).hasClass("bottom-selected")) {
-        bottomCell.removeClass("top-selected");
-    }
-
-    if($(ele).hasClass("left-selected")) {
-        leftCell.removeClass("right-selected");
-    }
-
-    if($(ele).hasClass("right-selected")) {
-        rightCell.removeClass("left-selected");
-    }
-
-    $(ele).removeClass("selected top-selected bottom-selected left-selected right-selected");
 
 }
 
