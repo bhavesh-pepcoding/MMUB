b80ba4ca6d36c43c1dad6b706a3e8a0b66b25fd8
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-05-16 20:15:44 +0530

    ADD v26

--- DOM/excel-clone/script.js
+++ DOM/excel-clone/script.js
@@ -620,3 +620,40 @@ $(".left-scroller,.right-scroller").click(function(e){
     
     $(".sheet-tab.selected")[0].scrollIntoView();
 });
+
+$("#menu-file").click(function(e) {
+    let fileModal = $(`<div class="file-modal">
+                        <div class="file-options-modal">
+                            <div class="close">
+                                <div class="material-icons close-icon">arrow_circle_down</div>
+                                <div>Close</div>
+                            </div>
+                            <div class="new">
+                                <div class="material-icons new-icon">insert_drive_file</div>
+                                <div>New</div>
+                            </div>
+                            <div class="open">
+                                <div class="material-icons open-icon">folder_open</div>
+                                <div>Open</div>
+                            </div>
+                            <div class="save">
+                                <div class="material-icons save-icon">save</div>
+                                <div>Save</div>
+                            </div>
+                        </div>
+                        <div class="file-recent-modal"></div>
+                        <div class="file-transparent"></div>
+                    </div>`);
+    $(".container").append(fileModal);
+    fileModal.animate({
+        width: "100vw"
+    },300);
+    $(".close,.file-transparent").click(function(e) {
+        fileModal.animate({
+            width: "0vw"
+        },300);
+        setTimeout(() => {
+            fileModal.remove();
+        }, 250);
+    });
+});
